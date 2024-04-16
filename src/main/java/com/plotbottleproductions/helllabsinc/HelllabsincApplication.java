62e6f647c3ff9f2ddb5e0d31468869eca6948c83
SamuelPhilMS <kittylivvy00@gmail.com>
2024-04-16 08:27:27 -0500

    making an app from scratch

---
+++ src/main/java/com/plotbottleproductions/helllabsinc/HelllabsincApplication.java
@@ -0,0 +1,13 @@
+package com.plotbottleproductions.helllabsinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelllabsincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelllabsincApplication.class, args);
+	}
+
+}
