fec504894ac467001bfd51396dc9bc96715af005
SamuelPhilMS <kittylivvy00@gmail.com>
2024-05-24 15:59:07 -0500

    continually getting 500 errors for pages other than the home page

--- src/main/java/com/plotbottleproductions/helllabsinc/HelllabsincApplication.java
+++ src/main/java/com/plotbottleproductions/helllabsinc/HelllabsincApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelllabsincApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelllabsincApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(HelllabsincApplication.class, args);
 	}
 
 }
